1c4606c288a16974c35b91876ddde731a02701e6
ClayBattle <cbattle1@binghamton.edu>
2024-02-17 16:29:10 -0500

    Working video send

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_cors import CORS
 import requests
-
+import manimTest
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -12,6 +12,19 @@ CORS(app)  # This will enable CORS for all routes
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# Define a route and the associated function to handle requests to that route
+@app.route('/generate_video')
+def generate_video():
+    # Your Manim script to generate the MP4 file
+    scene = manimTest.SquareToCircle()
+    scene.render()
+
+    # Return the path to the generated MP4 file
+    return send_file('media\\videos\\1080p60\\SquareToCircle.mp4')
+
+
 
 # Run the Flask application
 if __name__ == '__main__':
---
+++ biddle9000/app/VideoPlayer.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+function VideoPlayer() {
+    const [mp4Url, setMp4Url] = useState('');
+    
+    // Define a function to send a request to the Flask backend
+    const sendRequestToFlask = async () => {
+        try {
+        // Make a GET request to the Flask backend
+        let response = await fetch('http://localhost:5000/generate_video');
+        let blob = await response.blob();
+        console.log(response)
+        
+        const videoUrl = URL.createObjectURL(blob);
+        console.log(`URL${videoUrl}`)
+        console.log(typeof(videoUrl))
+        // Set the URL of the received MP4 file
+        setMp4Url(videoUrl);
+        
+        }
+        catch(error)  {
+            console.error('Error fetching MP4 file:', error);
+        };
+    };
+  
+
+    return (
+        <div>
+            <button onClick={sendRequestToFlask} className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Test
+            on
+            </button>
+            <video key={mp4Url} width="640" height="480" controls>
+                <source src={mp4Url} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <button onClick={ () => console.log(mp4Url)} className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            log
+            </button>
+        </div>
+    );
+}
+
+export default VideoPlayer;
